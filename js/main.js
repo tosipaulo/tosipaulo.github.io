03ee26c924b5a2420595273208e2ae9cb3203bd6
Paulo Tosi <tosi.paulo@gmail.com>
2015-05-18 19:09:12 -0300

    Finalizando todo article versao mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 	var yPos = $(window);
 	console.log(yPos);
+
+	var $ppc = $('.progress-pie-chart'),
+    percent = parseInt($ppc.data('percent')),
+    deg = 360*percent/100;
+  if (percent > 50) {
+    $ppc.addClass('gt-50');
+  }
+  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+
+	
 });
