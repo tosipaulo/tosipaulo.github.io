2b1f9f1b730a49b6fc203ba5f3c3f6bd325f0adb
Paulo Tosi <tosi.paulo@gmail.com>
2015-09-17 21:04:16 -0300

    Index completa

--- controld/js/main.js
+++ controld/js/main.js
@@ -2,10 +2,26 @@ $(document).ready(function() {
 	//Menu
   var largura = $(window).width(); // largura do viewport
   var altura  = $(window).height();
+  var nav = $('.barra-menu-red');
 
-  var altura  = $(window).height(); // altura do viewport 
-  //$('.menu').before("<span class='nav-toggle'>Menu <i class='fa fa-bars'></i></span>");
-  //$('.nav-toggle').hide();
+  $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $(".scrollTop").fadeIn();
+        } else {
+            $(".scrollTop").fadeOut();
+        }
+
+
+    });
+
+    
+    $(".scrollTop").click(function() {
+        $('html, body').animate({
+          scrollTop : 0
+        }, 'show');
+      });
+
+
   $('.nav-toggle').on('click', function() {
     $('.nav-content').slideToggle(350);
     $('.nav-content').css('display', 'block');
